b6e529004cef235c7e1267a0c2acc355ee94eefe
ram-strativ <ramrachaim@gmail.com>
2025-11-20 15:22:45 +0600

    add animation file size text

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,9 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-screen items-center justify-start bg-zinc-50 font-sans dark:bg-black">
-      <div style={{ height: "50vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <div style={{ height: "50vh", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: 'column', gap: '1rem' }}>
         <h1 style={{ fontSize: "3rem", fontWeight: "bold" }}>Scroll Down to See the Animation</h1>
+        <p>Animation file size is : <span className='text-2xl text-green-700'>185KB</span></p>
       </div>
       <LottieScrollAnimation />
       <div style={{ height: "50vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
